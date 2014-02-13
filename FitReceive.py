f2ff2347166935c57e6816b218b4a8ff5435a88b
James Keeler <JamesKeeler@users.noreply.github.com>
2014-02-12 19:33:43 -0500

    Update FitReceive.py
    Since we are receiving images one line at a time, and each network packet contains the line number, this version stores the line of image data in a list at an index equal to the line number.
    
    Dropped packets mean the image is missing lines and the image is corrupt.  To solve this, we replace NULLs with neighboring elements.  This "heals" the image relatively well, especially if the number of dropped packets is small (e.g. less than 10%).

--- FitReceive.py
+++ FitReceive.py
@@ -3,36 +3,64 @@ from string import *
 import sys
 import time
 
-# The server and client MUST use the same port number
 host=''
 port = 42524
 s = socket(AF_INET,SOCK_DGRAM)
 s.bind((host,port))
 
-# The server and client MUST use the same buffer size
 addr = (host,port)
-buf = 8192
+buf = 5760
 
+# Add 4 bytes for the line number
+buf += 4
+
+# This list will hold the lines of image data
+lines = []
+for i in range(1082):
+    lines.insert(0, 'NULL')
+
+# Get header
 data,addr = s.recvfrom(buf)
-#print('Received File:',data.strip())
-#f = open(data.strip(),'wb')
 
-# Time has a decimal that we need to replace with '_'
+s.settimeout(2)
+count = 0
+try:
+    while(data):
+        count += 1
+        # The first 4 bytes contain the line number
+        lineNum = int(data[:4])
+        # Insert the image information into the list at index lineNum
+        lines[lineNum] = data[4:]        
+
+        # Get next line of the image
+        data,addr = s.recvfrom(buf)
+
+except timeout:
+    s.close()    
+    print('Transmission complete. Received '+ str(count) + ' packets.')
+
+count = 0
+# NULL values in the list mean we dropped packets. We will replace the NULLs 
+# with the list element neighbor.
+# We are done when there are no more NULLs in the list.
+done = False
+while (not done):
+    try:
+        # Index throws a ValueError exception when the search token isn't found
+        i = lines.index('NULL')
+        lines[i] = lines[i-1]
+        count += 1
+    except ValueError:
+        done = True
+
+print('Repaired ' + str(count) + ' lines.')
+
+# Epoch time will be used to create file name
 t = str(time.time())
-#table = str.maketrans('.', '_')
 
 # File name will be epoch time .bmp
 filename = t + '.bmp'
 f = open(filename,'wb')
-
-data,addr = s.recvfrom(buf)
-
-try:
-    while(data):
-        f.write(data)
-        s.settimeout(2)
-        data,addr = s.recvfrom(buf)
-except timeout:
-    f.close()
-    s.close()
-    print('Transmission complete.')
+for line in lines:
+    f.write(line)
+f.close()
